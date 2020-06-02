dc9a7febcfd58e35c43121684ddd0faaa32fba62
C-Pher <a.komisssarov@gmail.com>
2020-06-03 00:25:14 +0300

    1

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 __author__ = 'Andrey Komissarov'
 __date__ = '06.2020'
 
+import json
+import warnings
+from concurrent.futures.thread import ThreadPoolExecutor
+
+import requests
+
+warnings.filterwarnings('ignore')
 
 
+idrac_username = 'root'
+idrac_password = 'rds123RDS'
+
+
+def get_current_power_state(ip):
+    url = f'https://{ip}/redfish/v1/Systems/System.Embedded.1/'
+    try:
+        response = requests.get(url, verify=False, auth=(idrac_username, idrac_password))
+        data = response.json()
+
+        with open('inventory.txt', 'a') as f:
+            f.write(f'iDRAC IP: {ip}\n')
+            f.write(f'HostName: {data["HostName"]}\n')
+            f.write(f'MemorySummary: {data["MemorySummary"]["TotalSystemMemoryGiB"]} GB\n')
+            f.write(f'Model: {data["Model"]}\n')
+            f.write(f'PartNumber: {data["PartNumber"]}\n')
+            f.write(f'SKU: {data["SKU"]}\n')
+            f.write(f'SerialNumber: {data["SerialNumber"]}\n')
+            f.write(f'Status: {data["Status"]["Health"]}\n\n')
+
+        # print(data)
+        print(f'iDRAC IP: {ip}')
+        print(f'HostName: {data["HostName"]}')
+        print(f'Manufacturer: {data["Manufacturer"]}')
+        print(f'MemorySummary: {data["MemorySummary"]["TotalSystemMemoryGiB"]} GB')
+        print(f'Model: {data["Model"]}')
+        print(f'PartNumber: {data["PartNumber"]}')
+        print(f'SKU: {data["SKU"]}')
+        print(f'SerialNumber: {data["SerialNumber"]}')
+        print(f'Status: {data["Status"]["Health"]}')
+        print('============')
+
+    except requests.exceptions.ConnectionError:
+        print(f'Host {ip} is unreachable or failed to login!')
+    except json.decoder.JSONDecodeError:
+        print('Cannot decode response.')
+
+
+with open('ip') as file:
+    ips = [ip.strip() for ip in file.readlines()]
+
+    with ThreadPoolExecutor(max_workers=128) as pool:
+        for _ in pool.map(get_current_power_state, ips):
+            pass
